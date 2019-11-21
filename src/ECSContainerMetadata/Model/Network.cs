08fd07e1e1f30b85b47f1a7262d39ae170c8cdc0
Renato Golia <renato.golia@educations.com>
2019-11-21 20:13:00 +0100

    First commit

---
+++ src/ECSContainerMetadata/Model/Network.cs
@@ -0,0 +1,8 @@
+﻿namespace EMG.Extensions.Configuration.Model {
+    public class Network
+    {
+        public string NetworkMode { get; set; }
+
+        public string[] IPv4Addresses { get; set; }
+    }
+}
